22b0f65de044d7feed703a37d25c8fa8ea25f051
WessleyAaron <37811241+WessleyAaron@users.noreply.github.com>
2018-04-06 13:12:37 -0500

    upload Pg 71 Number 9

---
+++ Pg 71 Number 9/Pg 71 Number 9/Source.cpp
@@ -0,0 +1,15 @@
+//Crispies Bagels and Bites
+//4-6-18
+//Author: Aaron Wessley
+
+#include <iostream>
+
+
+int main()
+{
+	double bagels = 0.99;
+	double donuts = 0.75;
+
+	system("pause");
+	return 0;
+}
